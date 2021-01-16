2a64da28a544a0ce074f6dc86096861f2d0356f3
Liam <muirliam23@gmail.com>
2021-01-16 14:25:45 -0500

    added cors_headers, implemented models and created empty database

--- back_end/websitefinalproject/websiteFinalProjectAPI/views.py
+++ back_end/websitefinalproject/websiteFinalProjectAPI/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets          
+from .serializers import AuthorSerializer, QuoteSerializer
+from .models import Author, Quote
+
+class AuthorView(viewsets.ModelViewSet):
+    serializer_class = AuthorSerializer
+    queryset = Author.objects.all()
+
+class QuoteView(viewsets.ModelViewSet):
+    serializer_class = QuoteSerializer
+    queryset = Quote.objects.all()
 
 # Create your views here.
