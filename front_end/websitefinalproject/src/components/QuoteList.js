c4868ea18ea8e66e24d6e888e6d72b1e2bc7e2c9
Liam <muirliam23@gmail.com>
2021-02-02 12:06:49 -0500

    updated readme

--- front_end/websitefinalproject/src/components/QuoteList.js
+++ front_end/websitefinalproject/src/components/QuoteList.js
@@ -4,10 +4,11 @@ import {Layout, Space, List} from 'antd'
 const {Header} = Layout
 
 const QuoteList = ({signedIn,quote}) => {
+    console.log(quote)
     return (
         <Space direction='vertical' align='left'>
-            <h2>{quote.text}</h2>
-            <p>-{quote.author}</p>
+            <h2>{quote ? quote.text : <></>}</h2>
+            <p>-{quote ? quote.author : <></>}</p>
         </Space>
     )
 }
